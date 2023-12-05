d363e322b8f137d25b60a6b7cc3c6700842d7dfd
Yurammba <yuriy.storozhev@mail.ru>
2023-12-05 12:44:50 +0700

    компонент для отображения только 'любимых' видео

---
+++ src/app/rxjs/rxjs-favorite/rxjs-favorite.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-favorite',
+  templateUrl: './rxjs-favorite.component.html',
+  styleUrls: ['./rxjs-favorite.component.scss']
+})
+export class RxjsFavoriteComponent implements OnInit {
+  favoritesData: any[] = [];
+  ngOnInit(): void {
+    const savedData = localStorage.getItem('saveData');
+    if (savedData) {
+      this.favoritesData = JSON.parse(savedData);
+    }
+  }
+
+  removeFromFavorites(index: number): void {
+    this.favoritesData.splice(index, 1);
+    localStorage.setItem('saveData', JSON.stringify(this.favoritesData));
+  }
+}
