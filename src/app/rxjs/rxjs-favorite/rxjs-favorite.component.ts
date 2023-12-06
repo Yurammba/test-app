63d0e51dfa1ecb42b01b56ac0d0bea125f5b59ef
Yurammba <yuriy.storozhev@mail.ru>
2023-12-06 20:56:18 +0700

    Обновлен вывод данных в шаблон

--- src/app/rxjs/rxjs-favorite/rxjs-favorite.component.ts
+++ src/app/rxjs/rxjs-favorite/rxjs-favorite.component.ts
@@ -7,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RxjsFavoriteComponent implements OnInit {
   favoritesData: any[] = [];
+
   ngOnInit(): void {
     const savedData = localStorage.getItem('saveData');
     if (savedData) {
