5143d7b28bf74c5051476000739640fc64307a13
Yurammba <yuriy.storozhev@mail.ru>
2023-12-06 20:57:35 +0700

    Обновлен вывод данных в шаблон

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {YoutubeService} from "../service/youtube.service";
-import {concatMap, map} from "rxjs";
+import {Subscription} from "rxjs";
 import {Router} from "@angular/router";
 
 @Component({
@@ -8,101 +8,42 @@ import {Router} from "@angular/router";
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.scss']
 })
-export class RxjsComponent implements OnInit {
-  favorites = false;
-  videoThumbnails: string[] = [];
-  title: string [] = [];
-  currentPageToken: string | undefined ;
-  allVideoThumbnails: string[] = [];
-  cards: { url: string; title: string; favorites: boolean; }[] = [];
-  videosPerPage = 6;
+export class RxjsComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  videos = this.youtubeService;
 
   constructor(
     private youtubeService: YoutubeService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
-    const storedData = JSON.parse(localStorage.getItem('saveData') || '[]');
-    console.log('Stored Data:', storedData);
-
-    this.youtubeService.getHomePageVideos("", this.videosPerPage).subscribe((data: any) => {
-      this.videoThumbnails = data.items.map((item: any) => item.snippet.thumbnails.high.url);
-      this.title = data.items.map((item: any) => item.snippet.localized.title);
-
-      this.allVideoThumbnails = [...this.videoThumbnails];
-
-      // Проверяем наличие сохраненных данных в localStorage
-      if (storedData.length > 0) {
-        this.allVideoThumbnails.forEach((url, index) => {
-          const savedData = storedData.find((item: any) => item.title === this.title[index]);
-          this.cards.push({
-            url: url,
-            title: this.title[index],
-            favorites: savedData ? savedData.favorites : false
-          });
-        });
-      } else {
-        // Если сохраненных данных нет, инициализируем массив cards без favorites
-        this.allVideoThumbnails.forEach((url, index) => {
-          this.cards.push({url: url, title: this.title[index], favorites: false});
-        });
-      }
-
-      this.currentPageToken = data.nextPageToken;
-    });
+    this.youtubeService.store();
   }
-
 
   loadMoreVideos() {
-    if (this.currentPageToken) {
-      this.youtubeService.getHomePageVideos(this.currentPageToken, this.videosPerPage).subscribe((data: any) => {
-        console.log('Received additional videos:', data);
-        const additionalThumbnails = data.items.map((item: any) =>
-          item.snippet.thumbnails.high.url);
-        const additionalTitles = data.items.map((item: any) => item.snippet.localized.title);
-        console.log('Additional Thumbnails:', additionalThumbnails);
-
-        additionalTitles.forEach((title: string, index: number) => {
-          const url = additionalThumbnails[index];
-          const existingCard = this.cards.find(card => card.title === title);
-          if (existingCard) {
-            existingCard.favorites = true;
-          } else {
-            this.cards.push({url: url, title: title, favorites: false});
-          }
-        });
-
-        // Обновляем токен следующей страницы
-        this.currentPageToken = data.nextPageToken;
-      });
-    }
+    this.youtubeService.loadMoreVideos();
   }
 
-  toggleFavorite(index: number) {
-    const card = this.cards[index];
+  toggleFavorite(card: any) {
     card.favorites = !card.favorites;
-
-    let storedData: any[] = JSON.parse(localStorage.getItem('saveData') || '[]');
-
-    if (!Array.isArray(storedData)) {
-      storedData = [];
-    }
-
-    if (!card.favorites) {
-      storedData = storedData.filter((item: any) => item.title !== card.title);
-    } else {
+    let storedData: any[] = JSON.parse(localStorage.getItem('saveData')!) || [];
+    if (card.favorites) {
       const existingItemIndex = storedData.findIndex((item: any) => item.title === card.title);
       if (existingItemIndex !== -1) {
         storedData[existingItemIndex].favorites = true;
       } else {
-        storedData.push({ ...card });
+        storedData.push({...card});
       }
+    } else {
+      storedData = storedData.filter((item: any) => item.title !== card.title);
     }
-
     localStorage.setItem('saveData', JSON.stringify(storedData));
-    console.log('Сохраненные данные в LocalStorage:', storedData);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.youtubeService.resetData();
   }
 
   favorite() {
